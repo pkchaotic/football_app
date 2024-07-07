1dd43f451c053bab7c978322bd7f36a4b864477a
Phil Krispin <p.krispin@web.de>
2024-07-07 14:43:00 +0200

    add style for login

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -44,82 +44,94 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.sports_soccer,
-                  size: 100.0,
-                  color: Colors.blueAccent,
+      body: Container(
+        color: Colors.green[400], // Set a default background color or make it configurable
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Card(
+                elevation: 10.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
                 ),
-                SizedBox(height: 20.0),
-                Text(
-                  'Welcome to Football App',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.sports_soccer,
+                        size: 100.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(height: 20.0),
+                      Text(
+                        'Welcome to Football App',
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.email),
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.lock),
+                        ),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 10.0),
+                      DropdownButtonFormField<String>(
+                        value: role,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.person),
+                        ),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            role = newValue!;
+                          });
+                        },
+                        items: <String>['agent', 'coach']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                        onPressed: login,
+                        child: Text('Login'),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 50),
+                          textStyle: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextButton(
+                        onPressed: () {
+                          // Implement the forgot password functionality
+                        },
+                        child: Text('Forgot Password?'),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 20.0),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.email),
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock),
-                  ),
-                  obscureText: true,
-                ),
-                SizedBox(height: 10.0),
-                DropdownButtonFormField<String>(
-                  value: role,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.person),
-                  ),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      role = newValue!;
-                    });
-                  },
-                  items: <String>['agent', 'coach']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-                SizedBox(height: 20.0),
-                ElevatedButton(
-                  onPressed: login,
-                  child: Text('Login'),
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(double.infinity, 50),
-                    textStyle: TextStyle(fontSize: 18),
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                TextButton(
-                  onPressed: () {
-                    // Implement the forgot password functionality
-                  },
-                  child: Text('Forgot Password?'),
-                ),
-              ],
+              ),
             ),
           ),
         ),
